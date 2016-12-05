dc7a2f5d01b8ba0c4ee98df280e46f2312a5318e
yang.liu <yang.liu@lianshang.com>
2016-12-05 11:08:07 +0800

    fix

---
+++ JvmDemo/src/main/java/jvmLearn/outOfMemoryError/_005_TestAllocation.java
@@ -0,0 +1,11 @@
+package jvmLearn.outOfMemoryError;
+
+/**
+ * Created by lianshang on 2016/12/5.
+ */
+public class _005_TestAllocation {
+
+    public static void main(String[] args) {
+
+    }
+}
