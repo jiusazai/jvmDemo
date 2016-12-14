90caee00a43041bc020fc2b6a44e6fff82f6cfa9
yang.liu <yang.liu@lianshang.cn>
2016-12-14 18:47:34 +0800

    fix

---
+++ JvmDemo/src/main/java/jvmLearn/outOfMemoryError/_008_ClassCode.java
@@ -0,0 +1,9 @@
+package jvmLearn.outOfMemoryError;
+
+public class _008_ClassCode {
+    public static void main(String[] args) {
+        int a = 1 + 1;
+        int b = 2;
+        int c = a + b;
+    }
+}
